20411dd0fed1cf9ea74b250fbb1c3bf8681809f5
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-05-18 16:11:54 -0400

    cambios2 eva2

--- front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
+++ front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
@@ -20,7 +20,7 @@ export const PruebaCards = () => {
           }}
         />
         <CardComponent
-          title="Tabla de consultas despachos"
+          title="Tabla de consultas"
           description="Consulta los despachos realizados, modifica los registros de intentos o cierra la orden"
           buttonText="Consultar"
           onClick={() => {
